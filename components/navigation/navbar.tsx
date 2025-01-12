35ba9df62d2c0e08b67cad7dc2e8744eb6dda785
vanhongha <honghaa8@gmail.com>
2025-01-12 17:47:11 +0900

    display user's avatar (this time display default avatar only)

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router";
 import { destroyCookie } from "nookies";
+import { useContext } from "react";
 
 import { ACCESS_TOKEN_KEY } from "@/constant/auth";
+import { UserContext } from "@/store/user-context";
 
 export function Navbar() {
+  const userCtx = useContext(UserContext);
   const router = useRouter();
 
   const onSignOutHandler = (e: React.MouseEvent<HTMLElement>) => {
@@ -111,27 +114,12 @@ export function Navbar() {
         </div>
         <div>
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="h-10 rounded-full bg-cyan-300"
             data-dropdown-toggle="dropdownInformation"
             id="dropdownInformationButton"
             type="button"
           >
-            Dropdown header{" "}
-            <svg
-              aria-hidden="true"
-              className="w-2.5 h-2.5 ms-3"
-              fill="none"
-              viewBox="0 0 10 6"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
+            <img alt="Avatar" className="h-full " src="/default-avatar.png" />
           </button>
 
           {/*Dropdown menu*/}
@@ -140,8 +128,8 @@ export function Navbar() {
             id="dropdownInformation"
           >
             <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-              <div>Bonnie Green</div>
-              <div className="font-medium truncate">name@flowbite.com</div>
+              <div>{userCtx.user.getFullName()}</div>
+              <div className="font-medium truncate">{userCtx.user.email}</div>
             </div>
             <ul
               aria-labelledby="dropdownInformationButton"
