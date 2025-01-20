db7b23340436c4c74c8ff91ddfd88e2dd7c74b2e
vanhongha <honghaa8@gmail.com>
2025-01-20 23:46:13 +0900

    add input post component
    in user profile page add input post component

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,20 +1,13 @@
-import { useRouter } from "next/router";
-import { useCookies } from "react-cookie";
-
 import MenuTab from "../../components/userProfile/menuTab";
 import ProfileHeader from "../../components/userProfile/profileHeader";
 
-import { USER_ID_KEY } from "@/constant/auth";
 import DefaultLayout from "@/layouts/default";
 import "../../styles/userProfile.css";
 import ProfileNavMenu from "@/components/userProfile/customNavMenu/profileNavMenu";
+import InputPost from "@/components/userProfile/inputPost";
+import InformationAlert from "@/components/alerts/informationAlert";
 
 export default function Home() {
-  const router = useRouter();
-  const { username } = router.query;
-  const [cookies] = useCookies([USER_ID_KEY]);
-  const myUserId = cookies[USER_ID_KEY];
-
   return (
     <DefaultLayout>
       <div className="max-w-screen-xl w-full mx-auto">
@@ -22,8 +15,11 @@ export default function Home() {
         <MenuTab />
         <div className="flex mt-6">
           <ProfileNavMenu />
-          <div className="px-5">
-            Hello {username} {myUserId}
+          <div className="pl-5 grow">
+            <InputPost />
+            <div className="mt-5">
+              <InformationAlert message="You don't have any post." />
+            </div>
           </div>
         </div>
       </div>
